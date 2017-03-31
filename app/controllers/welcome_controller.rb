f95abbefb2cec961a80e0a41cc8dda3a96c94bad
allcrz <allcrz@gamil.com>
2017-03-31 08:19:02 -0700

    create group index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+#  def index
+#   flash[:notice] = "allcrz 早安！你好！"
+#   flash[:alert] = "allcrz 晚安！该睡了！"
+#   flash[:warning] = "allcrz 这是 warning 信息！"
+#  end
 end
